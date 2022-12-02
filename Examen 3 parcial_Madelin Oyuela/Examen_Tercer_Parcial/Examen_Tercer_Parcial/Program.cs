fa3dc728188b235bbd5cf126781d6086d9eab467
Madelin-Oyuela <117136280+Madelin-Oyuela@users.noreply.github.com>
2022-12-01 20:22:49 -0600

    Add files via upload

---
+++ Examen 3 parcial_Madelin Oyuela/Examen_Tercer_Parcial/Examen_Tercer_Parcial/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_Tercer_Parcial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static int factorial(int n)
+        {
+            int f, i;
+            f = 1;
+            if (n==0)
+            {
+                return 1;
+            }
+            else
+            {
+                for ( i = 1;  i <= n;  i++)
+                {
+                    f = f * i;
+                }
+                return f;
+            }
+        }
+
+    }
+}
---
+++ Examen 3 parcial_Madelin Oyuela/Examen_Tercer_Parcial_Ejercicio2/Examen_Tercer_Parcial_Ejercicio2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_Tercer_Parcial_Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(mcd(5,60));
+            Console.Read();
+        }
+
+        public static int mcd(int a,int b)
+        {
+            while (a!=b)
+            {
+                if (a>b)
+                {
+                    a = a - b;
+                }
+                else
+                {
+                    b = b - a;
+                }
+            }
+            return a;
+        }
+    }
+}
